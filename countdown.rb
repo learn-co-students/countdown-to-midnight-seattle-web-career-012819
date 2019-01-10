3905699d27272a3038030c466e6e422f7046f6bc
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-10 04:14:34 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num_seconds)
+  while num_seconds > 0
+    puts "#{num_seconds} SECOND(S)!"
+    num_seconds -= 1
+  end
+  
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num_seconds)
+  while num_seconds > 0
+    puts "#{num_seconds} SECOND(S)!"
+    num_seconds -= 1
+    sleep 1
+  end
+  
+  return "HAPPY NEW YEAR!"
+end 
